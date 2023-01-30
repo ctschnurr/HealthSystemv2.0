6977643ef0983083ce3517dc8ad4cee609356f80
ctschnurr <113189402+ctschnurr@users.noreply.github.com>
2023-01-30 09:52:02 -0400

    Initial commit

---
+++ HealthSystemv2.0/Player.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystemv2._0
+{
+    internal class Player
+    {
+        // fields
+
+        int health = 100;
+        int healthMax = 100;
+        int lives = 3;
+
+        public void TakeDamage(int harm)
+        {
+            if (health == 0)
+            {
+                Console.WriteLine("Player is DEAD!");
+            }
+
+            else
+            {
+                health -= harm;
+
+                if (health <= 0)
+                {
+                    Console.WriteLine("Health has dropped to zero! Player has DIED!");
+                    health = 0;
+                    lives--;
+
+                    if (lives == 0)
+                    {
+                        Console.WriteLine("Player is out of lives!");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Player took " + harm + " damage! Health is now " + health + "!");
+                }
+            }
+        }
+
+        public void Heal(int heal)
+        {
+            if (health == 0)
+            {
+                Console.WriteLine("Player is dead and cannot be healed!");
+            }
+
+            else if (health != 0)
+            {
+                if (health == healthMax)
+                {
+                    Console.WriteLine("Health is already full!");
+                }
+
+                else if (health + heal >= healthMax)
+                {
+                    Console.WriteLine("Health is restored to full!");
+                    health = healthMax;
+                }
+
+                else
+                {
+                    health += heal;
+                    Console.WriteLine("Player is healed for " + heal + " points! Health is now " + health + ".");
+                }
+            }
+        }
+
+        public void Revive()
+        {
+            if (health != 0)
+            {
+                Console.WriteLine("Player does not need to be revived, added 1 life!");
+                lives++;
+            }
+
+            else if (health == 0)
+            {
+                Console.WriteLine("Player has been revived!");
+                health = healthMax;
+            }
+        }
+        public void ShowHUD()
+        {
+            Console.WriteLine("Player Stats:");
+            Console.WriteLine();
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Lives: " + lives);
+        }
+    }
+}
---
+++ HealthSystemv2.0/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystemv2._0
+{
+    internal class Program
+    {
+        static Player player;
+        static ConsoleKeyInfo input;
+        static string storageString;
+        static int storageInt;
+
+        static void Main(string[] args)
+        {
+            player = new Player();
+
+            Console.WriteLine("Welcome to Health System v2.0");
+            Console.WriteLine("A player has been created for us to test with.");
+            Pause();
+
+            while (input.Key != ConsoleKey.D0)
+            {
+                Console.Clear();
+                Console.WriteLine("Please choose from the following options: ");
+                Console.WriteLine();
+                Console.WriteLine("(1) Display Player Stats");
+                Console.WriteLine("(2) Simulate Damage");
+                Console.WriteLine("(3) Simulate Heal");
+                Console.WriteLine("(4) Revive Player");
+                Console.WriteLine("(0) Quit");
+                Console.WriteLine();
+
+                input = Console.ReadKey(true);
+
+                if (input.Key == ConsoleKey.D1)
+                {
+                    player.ShowHUD();
+                    Pause();
+                }
+
+                if (input.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine("How much damage to simulate?");
+                    Console.Write("Enter harm: ");
+
+                    storageString = Console.ReadLine();
+                    bool isNumber = int.TryParse(storageString, out storageInt);
+
+                    if (isNumber == false)
+                    {
+                        Console.WriteLine("That is not a valid value for harm.");
+                        Pause();
+                    }
+
+                    if (isNumber == true)
+                    {
+                        player.TakeDamage(storageInt);
+                        Pause();
+                    }
+                }
+
+                if (input.Key == ConsoleKey.D3)
+                {
+                    Console.WriteLine("How much healing to simulate?");
+                    Console.Write("Enter heal: ");
+
+                    storageString = Console.ReadLine();
+                    bool isNumber = int.TryParse(storageString, out storageInt);
+
+                    if (isNumber == false)
+                    {
+                        Console.WriteLine("That is not a valid value for heal.");
+                        Pause();
+                    }
+
+                    if (isNumber == true)
+                    {
+                        player.Heal(storageInt);
+                        Pause();
+                    }
+                }
+
+                if (input.Key == ConsoleKey.D4)
+                {
+                    player.Revive();
+                    Pause();
+                }
+            }
+        }
+
+        static void Pause()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+        }
+    }
+}
